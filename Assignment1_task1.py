b7b52d5f23592d2e42fe3119b45816a64d429be2
Rishi977 <56195643+Rishi977@users.noreply.github.com>
2025-07-31 12:05:20 +0900

    Create Assignment1_task1.py

---
+++ Assignment1_task1.py
@@ -0,0 +1,34 @@
+########################################################
+##################  TASK 1 #############################
+########################################################
+#Taking numbers from the user
+
+number1 = int(input("Enter the first number: ")
+number2 = int(input("Enter the second number: ")
+
+#Performing arthmetic operations
+
+#ADDITION
+def addition(num1, num2):
+  sum = num1 + num2
+  print(f"The sum of {num1} and {num2} is: {sum}")
+
+#SUBTRACTION
+def subtraction(num1, num2):
+  sub = num1 - num2
+  print(f"The subtraction of {num1} and {num2} is: {sub}")
+
+#DIVIDISON
+def divide(num1, num2):
+  div = num1 / num2
+  print(f"The division of {num1} and {num2} is: {div}")
+
+def multiplication(num1, num2):
+  mul = num1 * num2
+  print(f"The sum of {num1} and {num2} is: {mul}")
+
+addition(num1=number1, num2=number2)
+subtraction(num1=number1, num2=number2)
+divide(num1=number1, num2=number2)
+mulitplication(num1=number1, num2=number2)
+
