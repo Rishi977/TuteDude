87bafbc62090ecb8abc0e38e09ad5feec554a6c5
Rishi977 <56195643+Rishi977@users.noreply.github.com>
2025-07-31 12:10:13 +0900

    Update Assignment1_Task2.py

--- Assignment1_Task2.py
+++ Assignment1_Task2.py
@@ -1,1 +1,15 @@
+########################################################
+##################  TASK 2 #############################
+########################################################
+#Taking First Name and Last Name from the user
 
+first_name = input("Enter the first name: ").captalize()
+last_name = input("Enter the last name: ").captalize()
+
+#Performing concatination operations
+
+def greetings(fname, lname):
+  full_name = fname + " " + lname
+  print("Hello, {full_name}! Welcome to the python programming.")
+
+greetings(fname=first_name, lname=last_name)
