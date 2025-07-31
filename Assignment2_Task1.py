3f1b1dfc1df2174ab3ed38c99b7c2dc696fce503
Rishi977 <56195643+Rishi977@users.noreply.github.com>
2025-07-31 13:50:16 +0900

    Update Assignment2_Task1.py

--- Assignment2_Task1.py
+++ Assignment2_Task1.py
@@ -1,1 +1,15 @@
+###################################################
+################### TASK 1 ########################
+###################################################
 
+#Checking whether the input number is even or odd.
+
+num = int(input("Enter a number: \n"))
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is an even number.")
+    else:
+        print(f"{number} is an odd number.")
+
+check_even_odd(number=num)
